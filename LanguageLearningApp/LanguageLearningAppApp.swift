e53d1abd85eac3df8232e59f55e4b2b15818ea15
Danil Anikin <mart4r.clearlist@gmail.com>
2024-08-12 16:58:11 +1000

    Initial Commit

---
+++ LanguageLearningApp/LanguageLearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageLearningAppApp.swift
+//  LanguageLearningApp
+//
+//  Created by Данил Аникин on 12.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct LanguageLearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
