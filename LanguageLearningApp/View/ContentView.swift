3426b17828aefcce1e6df9607e2c3137c6a286af
Danil Anikin <mart4r.clearlist@gmail.com>
2024-08-12 17:19:18 +1000

    create TabView

--- LanguageLearningApp/View/ContentView.swift
+++ LanguageLearningApp/View/ContentView.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-      
+      TabView {
+        Text("List")
+          .tag(1)
+          .tabItem {
+            VStack {
+              Image(systemName: "list.bullet")
+              Text("List")
+            }
+          }
+        Text("Words")
+          .tag(2)
+          .tabItem {
+            VStack {
+              Image(systemName: "textformat.abc")
+              Text("Words")
+            }
+          }
+        Text("Links")
+          .tag(3)
+          .tabItem {
+            VStack {
+              Image(systemName: "link")
+              Text("Links")
+            }
+          }
+      }
     }
 }
 
